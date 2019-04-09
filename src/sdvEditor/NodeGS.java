af1664c3b497158631e5582fd48582e8fe1fbd8c
AkaKSR <akaikarus@gmail.com>
2019-04-10 05:20:56 +0900

    Release 0.2 alpha
    First Release
    
    Supports platforms
    PC/iOS/Android

---
+++ src/sdvEditor/NodeGS.java
@@ -0,0 +1,36 @@
+package sdvEditor;
+
+import java.util.Scanner;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class NodeGS {
+	
+	public NodeGS() {
+		//System.out.println(" -- NodeGS constructor -- ");
+	}
+	
+	public static String nodegv(String gTag, Element eElement) {
+		//System.out.println(" -- nodegv() Start -- ");
+		NodeList nlList = eElement.getElementsByTagName(gTag).item(0).getChildNodes();
+		Node nValue = (Node) nlList.item(0);
+		
+		return nValue.getNodeValue();
+	}
+	
+	public static String nodesv(String gTag, Element eElement, String sca) {
+		//System.out.println(" -- nodesv() Start -- ");
+		NodeList nlList = eElement.getElementsByTagName(gTag).item(0).getChildNodes();
+		Node nValue = (Node) nlList.item(0);
+		
+		Scanner ns = new Scanner(sca);
+		String nss = ns.next();
+		ns.close();
+		nValue.setNodeValue(nss);
+		return nValue.getNodeValue();
+		
+	}
+
+}
